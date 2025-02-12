24d3dbbd171fedf441c5425046410d088ad4a243
Hebert W. Parreira <wandick.hebert98@gmail.com>
2025-02-12 08:48:21 -0300

    initial pnp integration

--- main/main.cpp
+++ main/main.cpp
@@ -9,11 +9,12 @@
 #include "ble_setup.h"
 #include "wifi_setup.h"
 #include "QMI8658_setup.h"
+#include "sample_azure_iot_pnp.c"
 
 extern "C" void app_main(void)
 {
     ESP_ERROR_CHECK(init_memory());
-    ESP_ERROR_CHECK(init_ble());
+    //ESP_ERROR_CHECK(init_ble());
     ESP_ERROR_CHECK( esp_netif_init() );
     ESP_ERROR_CHECK( esp_event_loop_create_default() );
     /*Allow other core to finish initialization */
@@ -23,4 +24,7 @@ extern "C" void app_main(void)
     Serial.begin(115200);
     setupQMI8658();
     ESP_ERROR_CHECK(init_wifi());
+    vStartDemoTask();
 }
+//E (17370) esp-tls-mbedtls: mbedtls_ssl_setup returned -0x7F00
+//I (17377) esp-tls-mbedtls: (FFFF8100): SSL - Memory allocation failed
